2a8b43a9ceb6bed267f3dfdc23b5f1bd84f9930a
Cooperator <StopDropCoop@gmail.com>
2023-01-11 19:58:53 -0500

    Adjusted webpack to point to /js inside Cooper-Website.

--- Cooper-Backend/webpack.config.js
+++ Cooper-Backend/webpack.config.js
@@ -2,12 +2,12 @@ const path = require('path')
 
 module.exports = {
     mode: 'development',
-    entry: './src/index.js',
+    entry: './js/index.js',
     // experiments: {
     //     topLevelAwait: true
     // },
     output: {
-        path: path.join(__dirname, '..', 'Cooper-Website'),
+        path: path.join(__dirname, '..', 'Cooper-Website', 'js'),
         filename: 'bundle.js'
     },
     watch: true
